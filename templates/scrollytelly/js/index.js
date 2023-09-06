feb54984ab77bc28bd905253e917c1ae0fdfdfc9
Mjumbe Poe <mjumbewu@gmail.com>
2023-09-06 13:32:42 -0400

    Update the scrollytelly template

---
+++ templates/scrollytelly/js/index.js
@@ -0,0 +1,19 @@
+import { SlideDeck } from './slidedeck.js';
+
+const map = L.map('map', {scrollWheelZoom: false}).setView([0, 0], 0);
+
+// ## The Base Tile Layer
+L.tileLayer('https://stamen-tiles.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg', {
+  attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.',
+}).addTo(map);
+
+// ## Interface Elements
+const slides = document.querySelectorAll('.slide');
+
+// ## The SlideDeck object
+const deck = new SlideDeck(slides, map);
+
+document.addEventListener('scroll', () => deck.calcCurrentSlideIndex());
+
+deck.preloadFeatureCollections();
+deck.syncMapToCurrentSlide();
